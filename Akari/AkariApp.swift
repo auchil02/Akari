5328df467f017e5776d1c586139f8c3bbf4926cf
auchil02 <auchil@ad.unc.edu>
2023-04-18 14:36:58 -0400

    Initial Commit

---
+++ Akari/AkariApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkariApp.swift
+//  Akari
+//
+//  Created by Adi Uchil on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AkariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
